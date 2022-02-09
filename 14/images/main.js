d7a334bb8ce4e0bc44daecde1ff0e133c6a8878d
TAHAWEI <70866079+tahawei@users.noreply.github.com>
2022-02-09 22:58:39 +0200

    circle progress css js

---
+++ 14/images/main.js
@@ -0,0 +1,6 @@
+const progressBar = document.querySelector(".value-container");
+const valueContainer = document.querySelector(".value-container");
+
+let progressValue = 0;
+let progressEndValue = 65;
+let speed = 50;
